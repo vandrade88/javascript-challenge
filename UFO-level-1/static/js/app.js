9be87b27fd67e6ebbb8f37be02dba7062c7d5e25
Valerie <vandrade07@yahoo.com>
2021-04-04 20:19:01 -0400

    updated js file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,12 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach((item) => {
+    var row = tbody.append("tr");
+    Object.entries(item).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    })
+})
