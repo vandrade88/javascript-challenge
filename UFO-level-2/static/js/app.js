f5d90d6835c89338f1bae8764a14d9fb6a375a1b
Valerie <vandrade07@yahoo.com>
2021-04-04 22:57:16 -0400

    created state input

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -5,8 +5,6 @@ var table = d3.select("tbody");
 
 // create function to create our table and append data to it
 function make_table(tableData) {
-    // remove HTML inside tbody object
-    table.html("");
     tableData.forEach((item) => {
         // append new rows into table
         var row = table.append("tr");
@@ -26,16 +24,13 @@ make_table(tableData);
 // select the button to create an event handler
 var button = d3.select("#filter-btn");
 
-// select the form to create an event handler
-var form = d3.select("form");
-
 // select the input elements and get the raw HTML node
 var inputDate = d3.select("#datetime");
 var inputCity = d3.select("#city");
+var inputState = d3.select("#state");
 
 // create event handlers 
 button.on("click", runEnter);
-form.on("submit", runEnter);
 
 // complete the event handler function for the form
 function runEnter() {
@@ -45,10 +40,12 @@ function runEnter() {
 
   // select the value from the input field that the user enters
   var inputValueDate = inputDate.property("value");
-  var inputValueCity = inputCity.property("value");
+  var inputValueCity = inputCity.property("value").toLowerCase();
+  var inputValueState = inputState.property("value").toLowerCase();
 
   console.log(inputValueDate);
   console.log(inputValueCity);
+  console.log(inputState);
 
   // make copy of original data so our filtered data function doesn't affect original
   var new_data = tableData;
@@ -56,9 +53,13 @@ function runEnter() {
   // function to filter data by datetime
   var filteredDate = new_data.filter(item => item.datetime == inputValueDate);
   var filteredCity = new_data.filter(item => item.city == inputValueCity);
-
+  var filteredState = new_data.filter(item => item.state == inputValueState);
+  
+  // remove HTML inside tbody object
+  table.html("");
 
   // call make_table function to create new table with filteredData
   make_table(filteredDate);
   make_table(filteredCity);
+  make_table(filteredState);
 }
